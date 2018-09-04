8a2be679c0e94ee0bb95164f2d171b99076387c3
Gabriel Silveira <gabi.msilveira@gmail.com>
2018-09-04 19:32:57 -0300

    Modified AvalicaoProfessor

--- models/AvaliacaoProfessor.js
+++ models/AvaliacaoProfessor.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const avaliacaoProfessorSchema = new mongoose.Schema({
-  aluno: { type: Aluno, required: true },
   professor: { type: Professor, required: true },
+  aluno: { type: Aluno, required: true },
   nota: { type: Number, required: true },
   comentario: { type: String, required: true },
   aula: { type: Aula, required: true },
