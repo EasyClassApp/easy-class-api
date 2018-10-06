cbda7674558cedb2402012e6552c0129bd424739
mathiasVoelckerPUCRS <mathias.voelcker@acad.pucrs.br>
2018-10-06 10:23:51 -0300

    criar get professor por id

--- controllers/user/professorController.js
+++ controllers/user/professorController.js
@@ -1,27 +1,38 @@
+/* eslint-disable */
 import httpStatus from 'http-status';
 import Professor from '../../models/Professor';
 
 export async function getProfessores(req, res) {
-    try {
-      const professores = await Professor.find();
-      return res.send(professores);
-    } catch (error) {
-      return res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .json({ error: 'Ocorreu um erro ao buscar a lista de professores' });
-    }
+  try {
+    const professores = await Professor.find();
+    return res.send(professores);
+  } catch (error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Ocorreu um erro ao buscar a lista de professores' });
   }
+}
+
+export async function getProfessor(req, res) {
+  try {
+    const professor = await Professor.findById(req.params.id);
+    return res.send(professor);
+  } catch (error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Ocorreu um erro ao buscar a lista de professores' });
+  }
+}
 
 export async function createProfessor(req, res) {
-    try {    
-      var professor = new Professor();
-      professor.set(req.body);
-      await professor.save();
-      return res.json(professor);
-    } catch (error) {
-      return res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .json({ error: 'Ocorreu um erro ao tentar cadastrar professor' });
-    
-    }
+  try {
+    const professor = new Professor();
+    professor.set(req.body);
+    await professor.save();
+    return res.json(professor);
+  } catch (error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Ocorreu um erro ao tentar cadastrar professor' });
+  }
 }
