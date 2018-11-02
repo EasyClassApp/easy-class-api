d5e34473fe72fd18aa4ac7a6e6010c0882e1dbe8
Rafael Companhoni <companhoni@gmail.com>
2018-11-01 22:18:26 -0300

    Package.json

--- infrastructure/database.js
+++ infrastructure/database.js
@@ -12,6 +12,5 @@ const connectionString = {
   },
 };
 
-console.log(`Connected to DB ${connectionString[process.env.NODE_ENV].databaseUri}`)
 const db = connectionString[process.env.NODE_ENV].databaseUri;
 mongoose.connect(db);
