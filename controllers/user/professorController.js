e3fac27d19ece61ae7a9c54262ef73ec8bc81c0d
Luiz Fernando Weschenfelder Cardoso <luiz.cardoso@acad.pucrs.br>
2018-11-07 19:54:23 -0200

    adicao rota pesquisa professores por materia

--- controllers/user/professorController.js
+++ controllers/user/professorController.js
@@ -1,11 +1,24 @@
 /* eslint-disable */
 import httpStatus from 'http-status';
 import Professor from '../../models/Professor';
+import Materia from '../../models/Materia';
 import User from "../../models/User";
 
 export async function getProfessores(req, res) {
   try {
     const professores = await Professor.find();
+    return res.send(professores);
+  } catch (error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Ocorreu um erro ao buscar a lista de professores' });
+  }
+}
+
+export async function getProfessoresByMateria(req, res) {
+  try {
+    var nomeMateria = String(req.params.materia);
+    const professores = await Professor.find({"materias.nome": nomeMateria});
     return res.send(professores);
   } catch (error) {
     return res
